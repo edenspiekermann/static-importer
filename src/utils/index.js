1e5d8fc3efad2d1513bf543e58e821c97c746864
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-08-02 18:31:30 +0200

    Removed unnecessary getApiUrl util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,8 @@
-const getApiUrl = require('./get-api-url');
 const parseResponse = require('./parse-response');
 const refreshDir = require('./refresh-dir');
 const template = require('./template');
 
 module.exports = {
-  getApiUrl,
   parseResponse,
   refreshDir,
   template
